5fa4eef6e3645612f82e506a5ebe405e2e4683cb
teralani <132224175+teralani@users.noreply.github.com>
2026-02-05 17:40:18 -0800

    fix url

--- quiz-page/app/[conference]/results/page.tsx
+++ quiz-page/app/[conference]/results/page.tsx
@@ -206,7 +206,7 @@ useEffect(() => {
                   })}
     
                 </div>
-                <Link href={`https://kingmun.org/committees/${committees[r.idx].acronym.replace("-", "").toLowerCase()}`} target="_blank">
+                <Link href={`https://kingmun.org/committees/${committees[r.idx].acronym.toLowerCase()}`} target="_blank">
                   <button 
                     className="w-full relative bottom-2 mt-7 rounded-lg p-3 hover:-translate-y-0.5  transition"
                     style={{ backgroundColor: `var(--color-${conferenceSlug}-primary)` }}
