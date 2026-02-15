79c13890f4c5f3503f99b9ccf35b4630d1538ee3
teralani <132224175+teralani@users.noreply.github.com>
2026-02-15 15:28:01 -0800

    vercel analytics + dependencies updated

--- quiz-page/app/layout.tsx
+++ quiz-page/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${monsterrat.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
